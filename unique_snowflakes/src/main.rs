64d6d8d3c24fa454387a935b869c258d4072bf4f
Kenneth Chau <Chau.Kenneth@pm.me>
2026-01-10 14:26:49 -0500

    update code to account for two array with unsigned integer

--- unique_snowflakes/src/main.rs
+++ unique_snowflakes/src/main.rs
@@ -1,25 +1,29 @@
 use std::env;
+use std::array;
 use std::fs::File;
 use std::io::{self, BufRead, LineWriter, Write};
 use std::path::Path;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
-    let file_path = &args[1];
+    let test_arr: [u32; 5] = [1,2,3,1,5];
 
-    // Create a vector to store the file_content
-    let mut file_content = Vec::new();
+    identical_right(&test_arr, &test_arr, 0);
 
-    /* This following line read the input using the buffer function  and store them into a vector*/
-    if let Ok(lines) = read_lines(file_path) {
-        for line in lines.map_while(Result::ok){
-            let _ = &file_content.push(line);
-        }
-    }
-    let first_line: Vec<&str> = file_content[0].split_whitespace().collect();
+    // let args: Vec<String> = env::args().collect();
 
-    let people_in_line: i32 = first_line[1].parse().expect("Not a valid number");
+    // let file_path = &args[1];
+
+    // // Create a vector to store the file_content
+    // let mut file_content = Vec::new();
+
+    // /* This following line read the input using the buffer function  and store them into a vector*/
+    // if let Ok(lines) = read_lines(file_path) {
+    //     for line in lines.map_while(Result::ok){
+    //         let _ = &file_content.push(line);
+    //     }
+    // }
+
 
 }
 
@@ -30,5 +34,9 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-/* */
+/* Check if two integers are the same */
+fn identical_right(snow1: &[u32], snow2: &[u32], start: u32) {
+    let (offset, snow2_index): (u32,u32);
+    return
+}
 
